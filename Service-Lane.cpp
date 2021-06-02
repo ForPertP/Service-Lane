050c763f3357309469f80294473dd9dc864baa20
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-03 00:32:43 +0900

    Update Service-Lane.cpp

--- Service-Lane.cpp
+++ Service-Lane.cpp
@@ -21,3 +21,19 @@ vector<int> serviceLane(int n, vector<vector<int>> cases, vector<int> width)
     
     return result;
 }
+
+vector<int> serviceLane1(int n, vector<vector<int>> cases, vector<int> width)
+{
+   vector<int> result;
+    
+    for (int i = 0; i < cases.size(); ++i)
+    {
+        int start = cases[i][0];
+        int end = cases[i][1];
+        
+        result.emplace_back(
+            *min_element(width.begin()+start, width.end()-width.size()+end+1));       
+    }
+    
+    return result;
+}
