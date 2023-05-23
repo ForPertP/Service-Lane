69c57149825384f1cdd014d2c869220fbfae91d9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-23 14:17:38 +0900

    Create Service-Lane.java

---
+++ Service-Lane.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'serviceLane' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY cases
+     */
+
+    public static List<Integer> serviceLane(int n, List<List<Integer>> cases, List<Integer> width) {
+    // Write your code here
+    }
+}
